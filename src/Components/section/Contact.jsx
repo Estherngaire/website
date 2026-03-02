18a4a8943dab825d705f45dbf631512b3d9f41c6
Estherngaire <essymso59@gmail.com>
2026-03-02 14:02:40 +0300

    backed url link

--- src/Components/section/Contact.jsx
+++ src/Components/section/Contact.jsx
@@ -6,7 +6,7 @@ function Contact(){
          email: e.target.email.value,
          message: e.target.message.value,  
         };
-        await fetch("http://localhost:5000/contact", {
+        await fetch("https://esther-muthoni.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
